34f00cfb7b58e8da9c775f24c02cc17f6d5413b8
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:17:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO series (id, title, author_id, subgenre_id)
-Values (1, "Star Wars: The Novel", 1, 1), (2, "Earth Wars: Not Enough Lasers", 2, 2);
+Values (1, "Star Wars", 1, 1), (2, "Earth Wars", 2, 2);
+
+INSERT INTO books (id, title, year, series_id)
+Values (1, )
