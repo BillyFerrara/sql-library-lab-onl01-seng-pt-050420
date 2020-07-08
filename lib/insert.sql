e42793c1b316fc7515fd69b193ec13d26ccbfd31
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:21:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,6 @@
-INSERT INTO series (id, title, author_id, subgenre_id)
-Values (1, "Star Wars", 1, 1), (2, "Earth Wars", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) Values 
+(1, "Star Wars", 1, 1), 
+(2, "Earth Wars", 2, 2);
 
 INSERT INTO books (id, title, year, series_id) Values
 (1, "Phantom Menace", 2001, 1),
@@ -8,3 +9,5 @@ INSERT INTO books (id, title, year, series_id) Values
 (4, "A New Hope", 2004, 1),
 (5, "Empire Strikes Back", 2005, 1),
 (6, "Return of the Jedi", 2006, 1);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
