c9788e4fc7e813ed55eb26c5d808f04f7f915e71
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:11:35 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,6 @@
 create table series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 
-create table subgenre (id PRIMARY KEY, name TEXT);
+create table subgenres (id PRIMARY KEY, name TEXT);
 
 create table authors (id PRIMARY KEY, name TEXT);
 
