8e270ede73c3293d43091c11bc1bfbb2dc73a03b
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:36:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,3 +29,19 @@ INSERT INTO authors (id, name) VALUES
 (2, "Leorge Gucas");
 
 INSERT INTO character_books (id, book_id, character_id) VALUES
+(1, 1, 1),
+(2, 1, ),
+(3, , ),
+(4, , ),
+(5, , ),
+(6, , ),
+(7, , ),
+(8, , ),
+(9, , ),
+(10, , ),
+(11, , ),
+(12, , ),
+(13, , ),
+(14, , ),
+(15, , ),
+(16, , );
