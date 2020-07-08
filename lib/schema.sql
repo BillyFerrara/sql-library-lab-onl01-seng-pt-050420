912390ffdf627ecd01df7ca8b0aae1a6823ac623
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:13:00 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,11 +1,11 @@
 create table series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 
-create table subgenres (id PRIMARY KEY, name TEXT);
+create table subgenres (id INTEGER PRIMARY KEY, name TEXT);
 
-create table authors (id PRIMARY KEY, name TEXT);
+create table authors (id INTEGER PRIMARY KEY, name TEXT);
 
-create table books (id PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+create table books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 
-create table characters (id PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+create table characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
 
-create table character_books (id PRIMARY KEY, book_id INTEGER, character_id INTEGER);
+create table character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);
