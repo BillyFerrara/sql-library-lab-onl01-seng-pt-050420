0ad844dab6488f88875e38f6cce8377f458ae37f
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:30:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -13,6 +13,9 @@ INSERT INTO books (id, title, year, series_id) Values
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 (1, "Luke", "I am a Jedi, like my father before me", "human", 1, 1),
 (2, "Leia", "Aren't you a little short to be a Stormtrooper", "human", 1, 1),
-(3, "Han", "I know", "human", 1, 1);
-(4, "Obi-Wan", "Strike me down and I'll become more powerful than you could ever have imagined", "human", 1, 1);
-(5, "Darth Vader", "No, I am your father")
+(3, "Han", "I know", "human", 1, 1),
+(4, "Obi-Wan", "Strike me down and I'll become more powerful than you could ever have imagined", "human", 1, 1),
+(5, "Darth Vader", "No, I am your father", "cyborg", 1, 1),
+(6, "Jabba The Hutt", "Obo watta no no", "hutt", 1, 1),
+(7, "Chewbacca", "AAAARRRGGHHHHHHH", "wookie", 1, 1),
+(8, "Lando", "How you doin', ya old pirate", "human", 1, 1);
