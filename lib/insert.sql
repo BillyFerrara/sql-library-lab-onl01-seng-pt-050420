0a8268ad547be5c2be180151e3be73da31c3f07e
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:21:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,3 +3,8 @@ Values (1, "Star Wars", 1, 1), (2, "Earth Wars", 2, 2);
 
 INSERT INTO books (id, title, year, series_id) Values
 (1, "Phantom Menace", 2001, 1),
+(2, "Attack of the Clones", 2002, 1),
+(3, "Revenge of the Sith", 2003, 1),
+(4, "A New Hope", 2004, 1),
+(5, "Empire Strikes Back", 2005, 1),
+(6, "Return of the Jedi", 2006, 1);
