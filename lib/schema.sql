b9b05a31f28b06b85e3f9efaedcf3d95de5b411b
Billy Ferrara <willfira717@gmail.com>
2020-07-08 20:06:24 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -5,3 +5,5 @@ create table subgenre (id PRIMARY KEY, name TEXT);
 create table authors (id PRIMARY KEY, name TEXT);
 
 create table books (id PRIMARY KEY, title TEXT, year INTEGER, series TEXT);
+
+create table characters (id PRIMARY KEY, name TEXT, )
